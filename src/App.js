309c346bac80c3f7b321c9a4b99c1d70ad8a67fe
David CELLIER <paduction@gmail.com>
2021-11-28 21:50:06 +0100

    feat: change some text

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Coucou, c'est <code>Paduction</code> et Reactjs a l'air pas mal.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://paduction.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          visite Paduction.com
         </a>
       </header>
     </div>
