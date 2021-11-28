6d6045dc6acff5f4fb37add94755fb31255d7731
David CELLIER <paduction@gmail.com>
2021-11-28 23:11:14 +0100

    feat: add jackie chan

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import Footer from './components/Footer';
+import logoJackie from './jackie.jpg'; // Tell webpack this JS file uses this image
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logoJackie} className="App-logo" alt="logo" />
         <p>
           Coucou, c'est <code>Paduction</code> et Reactjs a l'air pas mal.
         </p>
